4be147b1f25d09ecaecb5a3994c65316b3dfb51a
SuzannaW-web <suzannawallis@gmail.com>
2025-03-24 23:51:34 +0000

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "words go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 2,
     curor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "3o03ae1d8at693bf40d8fb4c10733055";
+  let context =
+    "You are a haiku expert. Your task is to create a haiku following the rules of Haiku 17 syllables written in 3 lines. Seperate each line with <br/>.  ";
+  let prompt = `Generate a haiku about${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
