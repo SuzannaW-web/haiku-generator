5298400bb2753bbacc09d494a744c7d5634dfaa9
SuzannaW-web <suzannawallis@gmail.com>
2025-03-29 18:42:46 +0000

    Updated CSS and fixed typo in JS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayPoem(response) {
     strings: response.data.answer,
     autoStart: true,
     delay: 40,
-    curor: "",
+    cursor: "",
   });
 }
 
@@ -12,13 +12,14 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "3o03ae1d8at693bf40d8fb4c10733055";
   let context =
-    "You are a haiku expert. Your task is to create a haiku following the rules of Haiku 17 syllables written in 3 lines. Seperate each line with <br/>.  ";
-  let prompt = `Generate a haiku about${instructionsInput.value}`;
+    "You are a haiku expert. Your task is to create a haiku following the rules of Haiku 17 syllables written in 3 lines. Seperate each line with <br/>. Add SheCodes AI at the bottom in side <cite> ";
+  let prompt = `Generate a haiku about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="loader">Generating a haiku about ${instructionsInput.value}</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
